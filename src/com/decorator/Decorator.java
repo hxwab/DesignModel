05289f415a65f0c9b8f54d9459e2bd3bbc909012
hxwab <hxwab2010@126.com>
2016-01-29 16:28:13 +0800

    add

--- src/com/decorator/Decorator.java
+++ src/com/decorator/Decorator.java
@@ -6,14 +6,14 @@ import java.util.List;
  *   装饰者模式的问题及解决:用装饰者实例化组件时,将增加代码的复杂度,一旦应用了装饰者模式,不只需要实例化组件,
  *   还要把组件包装进装饰者,而这样的装饰者有多少个是不确定的。这里可以应用工厂模式来实例化组件来简化操作。
  * @author csdc
- *
+ *使用代理模式，代理和真实对象之间的的关系通常在编译时就已经确定了，而装饰者能够在运行时递归地被构造。    
  */
 public class Decorator extends Component {
 
 	private Component component;
 	
 	public Decorator(Component component) {
-		this.component = component;
+		this.component = component;//多态，在编译期间不确定，运行期间确定
 	}
 
 
--- src/com/decorator/Test.java
+++ src/com/decorator/Test.java
@@ -3,7 +3,7 @@ package com.decorator;
 public class Test   {
 	
 	public static void main(String[] args) {
-		
+		//客户指定了装饰者需要装饰的是哪一个类
 		Component component = new DB_News(); //需要知道装饰的是那一个类
 		Decorator decorator = new ConcreteDecortor_AddAmount(component);
 		decorator = new ConcreteDecortor_AddRss(decorator);
