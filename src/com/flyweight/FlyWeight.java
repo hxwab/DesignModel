6c029b695f4c4c3f67a394347a4c085c9c592126
hxwab <hxwab2010@126.com>
2016-02-24 21:08:59 +0800

    add

---
+++ src/com/flyweight/FlyWeight.java
@@ -0,0 +1,10 @@
+package com.flyweight;
+/**
+ * 抽象类，抽象出公用的接口方法
+ * @author csdc
+ *
+ */
+public abstract class FlyWeight {
+
+	protected abstract void operation();
+}
---
+++ src/com/flyweight/FlyWeightFactory.java
@@ -0,0 +1,45 @@
+package com.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+/**
+ * 享元模式的工厂类，实现对象的共享
+ * @author csdc
+ *
+ */
+public class FlyWeightFactory {
+	
+	private Map<Object, FlyWeight> flyWeights = new HashMap<Object, FlyWeight>();
+	private FlyWeight flyWeight;
+	private static FlyWeightFactory factory ;
+	private static Object lock = new Object();
+	
+	public static FlyWeightFactory getInstance(){ //单例模式
+		if(factory==null){
+			synchronized (lock) {
+				if(factory==null){
+					factory = new FlyWeightFactory();
+				}
+			}
+		}
+		
+		return factory;
+	}
+	
+	public FlyWeight    getFlyWeight(Object obj) {
+		flyWeight = flyWeights.get(obj); //先在内存中查找看有无该对象，有直接返回，无新建并加入内存中（Map），保证共享对象
+		if(flyWeight==null){
+			flyWeight = new ConcreteFlyWeight((String) obj);
+			flyWeights.put(obj, flyWeight);
+		}
+		return flyWeight;
+	}
+	
+	public int getFlyWeightSize(){
+		return flyWeights.size();
+	}
+	
+	
+}
