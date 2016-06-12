f785327f0c8f7a27047b2516b5e0c5fd9e104516
hxwab <hxwab2010@126.com>
2016-06-12 21:27:33 +0800

    add

---
+++ src/com/observer/WeatherData.java
@@ -0,0 +1,50 @@
+package com.observer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WeatherData implements Subject {
+
+	private Set<Observer> observers  = new HashSet<Observer>();
+	private float temperature;
+	private float humidity;
+	private float pressure;
+	
+	
+	
+	
+	@Override
+	public void registerObserver(Observer observer) {
+		observers.add(observer); 
+	}
+
+	@Override
+	public void removeObserver(Observer observer) {
+		observers.remove(observers);
+		
+	}
+
+	@Override
+	public void notifyObservers() {
+	
+		for(Observer o :observers){
+			o.update(this);
+		}
+	}
+
+	
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public float getHumidity() {
+		return humidity;
+	}
+
+	public float getPressure() {
+		return pressure;
+	}
+	
+	
+
+}
