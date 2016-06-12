70e0c1750c37dc42ebdc365a8d6efab8d9b6eee8
hxwab <hxwab2010@126.com>
2016-06-12 21:25:59 +0800

    add

--- src/com/strategy/Calculator.java
+++ src/com/strategy/Calculator.java
@@ -1,6 +1,13 @@
 package com.strategy;
 
+import java.util.Observable;
 
+/**
+ * 上下文对象，持有策略接口
+ * 策略模式的本质：分离算法，选择实现。
+ * @author csdc
+ *
+ */
 public class Calculator {
 
 	private int x;
@@ -14,6 +21,7 @@ public class Calculator {
 		this.y = y;
 	}
 
+	 Observable
 	public Calculator(int x, int y, ICalcStrategy strategy) {
 		this(x,y);
 		this.strategy = strategy;
@@ -29,10 +37,5 @@ public class Calculator {
 		}
 		return calc(x, y);
 	}
-	
-	
-	
-	
-	
 	
 }
