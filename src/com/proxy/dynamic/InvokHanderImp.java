5169063ef4d9a12319093c2beb5be7d764e79bd5
hxwab <hxwab2010@126.com>
2016-06-12 21:25:17 +0800

    add

--- src/com/proxy/dynamic/InvokHanderImp.java
+++ src/com/proxy/dynamic/InvokHanderImp.java
@@ -2,22 +2,38 @@ package com.proxy.dynamic;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
+
+/**
+ * 反射拿到obj,代理invoke（）方法
+ * @author csdc
+ *
+ */
 public class InvokHanderImp implements InvocationHandler {
 
 	
-	private ElectricCar car;
+	private Object targetObject;
 	
-	public InvokHanderImp(ElectricCar car) {
-		this.car=car;
+	public InvokHanderImp(Object targetObject) {
+		this.targetObject=targetObject;
 	}
+	
+	public Object creatProxy(){
+		
+		return Proxy.newProxyInstance(targetObject.getClass().getClassLoader(), 
+				targetObject.getClass().getInterfaces(), this);
+		
+	}
+	
+	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args)
 			throws Throwable {
 		
 		
 		System.out.println("you are going to invoke " +method.getName());
-		method.invoke(car, null);
+		method.invoke(targetObject, args);
 		System.out.println(method.getName()+" invoke has been finshed");
 		
 		return null;
